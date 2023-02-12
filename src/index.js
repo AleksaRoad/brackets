4cc2b288492b3cdfbe59b387f7c2a0ee08cab9b7
AleksaRoad <aleksaroad@gmail.com>
2023-02-12 23:05:03 +0300

    refractor: syntaxis

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-
   const opens = Object.fromEntries(bracketsConfig);
   const stack = [];
   let leftBr = bracketsConfig.map((x) => x[0]);
 
-
   for (let i = 0; str.length > i; i++) {
-    const char = str[i]
-    if(leftBr.includes(char)){
-      if(char == opens[stack[stack.length - 1]]) stack.pop()
+    const char = str[i];
+    if (leftBr.includes(char)) {
+      if (char == opens[stack[stack.length - 1]]) stack.pop();
       else stack.push(char);
-    }
-
-    else if (char == opens[stack[stack.length - 1]]) {
-    stack.pop(char);
-    } else {
-      return false;
+    } else if (char == opens[stack[stack.length - 1]]) stack.pop(char);
+      else return false;
   }
-  };
-  return !stack.length
+  return !stack.length;
 };
