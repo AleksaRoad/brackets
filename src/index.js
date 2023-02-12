cd97f47ec81067a1d7adeec5313c3b3be5194286
AleksaRoad <aleksaroad@gmail.com>
2023-02-12 22:59:34 +0300

    new answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  const opens = Object.fromEntries(bracketsConfig);
+  const stack = [];
+  let leftBr = bracketsConfig.map((x) => x[0]);
+
+
+  for (let i = 0; str.length > i; i++) {
+    const char = str[i]
+    if(leftBr.includes(char)){
+      if(char == opens[stack[stack.length - 1]]) stack.pop()
+      else stack.push(char);
+    }
+
+    else if (char == opens[stack[stack.length - 1]]) {
+    stack.pop(char);
+    } else {
+      return false;
+  }
+  };
+  return !stack.length
+};
